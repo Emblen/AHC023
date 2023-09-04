37c50801193a0bb6bc71a19bc9403cf9a856f6f8
Emblen <89460730+Kemblen@users.noreply.github.com>
2023-09-04 21:45:42 +0900

    とりあえず

--- AHC023.cpp
+++ AHC023.cpp
@@ -12,11 +12,49 @@
 #define v2i vector<vector<int>>
 #define v2b vector<vector<bool>>
 #define v2ll vector<vector<long long>>
+#define v3b vector<vector<vector<bool>>>
 #define all(x) x.begin(),x.end()
 using namespace std;
 using ll = long long;
 
+struct vec2{
+    int y, x; 
+    bool operator<(const vec2 right) const {
+        return y == right.y ? x < right.x : y < right.y;
+    }
+};
+
+
+struct Feild{
+
+};
+
+struct Solver{
+    
+    Solver(){}
+
+    void solve(){
+
+    }
+};
+
+
 int main(){
+    int t, h, w, i0;
+    cin >> t >> h >> w >> i0;
+
+    v3b f(h-1, v2b(w-1, vb(4, false)));
+    for(int i=0; i<h-1; i++){
+        for(int j=0; j<w-1; j++){
+
+        }
+    }
+    for(int i=0; i<h-1; i++){
+        for(int j=0; j<w-1; j++){
+            char c; cin >> c;
+            
+        }
+    }
     
     return 0;
 }
