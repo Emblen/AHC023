8e1482f4dacedf6d485be064a9bc823dbf4410fb
Kemblen <89460730+Kemblen@users.noreply.github.com>
2023-09-04 22:04:40 +0900

    入力の受け取り

--- AHC023.cpp
+++ AHC023.cpp
@@ -17,6 +17,10 @@
 using namespace std;
 using ll = long long;
 
+struct crop{
+    int s, d;
+};
+
 struct vec2{
     int y, x; 
     bool operator<(const vec2 right) const {
@@ -43,18 +47,39 @@ int main(){
     int t, h, w, i0;
     cin >> t >> h >> w >> i0;
 
-    v3b f(h-1, v2b(w-1, vb(4, false)));
+    //{N, E, S, W}
+    v3b f(h-1, v2b(w-1, vb(4, 0)));
     for(int i=0; i<h-1; i++){
-        for(int j=0; j<w-1; j++){
-
-        }
+        f[i][0][3] = 1;
+        f[i][w-2][1] = 1;
+    }
+    for(int i=0; i<w-1; i++){
+        f[0][i][0] = 1;
+        f[h-2][i][2] = 1;
     }
     for(int i=0; i<h-1; i++){
         for(int j=0; j<w-1; j++){
             char c; cin >> c;
-            
+            if(c=='1'){
+                f[i][j][2] = 1;
+                if(i!=h-1) f[i+1][j][0] = 1;
+            }
         }
     }
+    for(int i=0; i<w-1; i++){
+        for(int j=0; j<h-1; j++){
+            char c; cin >> c;
+            if(c=='1'){
+                f[j][i][1] = 1;
+                if(i!=h-1) f[j][i+1][3] = 1;
+            }
+        }
+    }
+    int k; cin >> k;
+    vector<crop> cp(k);
+    for(int i=0; i<k; i++) cin >> cp[i].s >> cp[i].d;
+
+    
     
     return 0;
 }
